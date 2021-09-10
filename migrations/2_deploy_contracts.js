77c37a75604289b9cf3908a75219a338b8b0f4f0
Christian Mora <christmo99@gmail.com>
2021-09-09 23:56:43 -0500

    feat: configuration for volcano token inicial

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var VolcanoToken = artifacts.require("./VolcanoToken.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(VolcanoToken);
 };
